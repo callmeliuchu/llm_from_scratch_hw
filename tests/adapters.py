9faf229d6bed82d703b8debf3e52a2e986dc79e2
liuchu <1371772034@qq.com>
2025-08-07 19:57:22 +0800

    update

--- tests/adapters.py
+++ tests/adapters.py
@@ -219,7 +219,10 @@ def run_rope(
     Returns:
         Float[Tensor, " ... sequence_length d_k"]: Tensor with RoPEd input.
     """
-    raise NotImplementedError
+    # raise NotImplementedError
+    from transformer import RotaryPositionalEmbedding
+    rope = RotaryPositionalEmbedding(d_k=d_k,theta=theta,max_seq_len=max_seq_len)
+    return rope(in_query_or_key,token_positions)
 
 
 def run_transformer_block(
